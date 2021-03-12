c14b716108112ea6f9c1d9f99fd9a64d4f9db08d
Ivan Telitsyn <itelitsyn@kisp.com>
2021-03-12 08:58:35 -0500

    Added script to generate PDF server-side.

---
+++ Scripts/generate-pdf.js
@@ -0,0 +1,16 @@
+﻿const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    //1 read our html page
+    let htmlContent = '';
+    const data = fs.readFile('example_template.html', 'utf8');
+
+    await page.setContent(data);
+    await page.pdf({ path: 'html.pdf', format: 'A4' });
+
+    await browser.close()
+})()
